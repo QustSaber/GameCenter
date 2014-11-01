e63eb21b9abd38c8fd9a74c64609ee71a9a9e857
lvhuantian <577071038@qq.com>
2014-11-01 12:44:53 +0800

    11/1

--- gen/com/anjoyo/gamecenter/R.java
+++ gen/com/anjoyo/gamecenter/R.java
@@ -22,6 +22,9 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int main_title_background=0x7f020001;
+        public static final int main_title_left=0x7f020002;
+        public static final int main_title_right=0x7f020003;
     }
     public static final class id {
         public static final int action_settings=0x7f080000;
@@ -33,6 +36,7 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int GameCenter=0x7f050003;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
